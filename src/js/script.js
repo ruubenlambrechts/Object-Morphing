e889418178ccc58f815dc03d241eeaaeb23f3717
RuubenL <ruuben.l@live.be>
2018-05-28 13:41:08 +0200

    ✨ THREE implementation

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,28 @@
+import * as THREE from 'three';
+import {TweenMax, Power0} from 'gsap/TweenMax';
 
+const canvas = document.querySelector(`.c`);
+const width = canvas.offsetWidth,
+  height = canvas.offsetHeight;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true
+});
+renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+renderer.setSize(width, height);
+renderer.setClearColor(0x000000);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 2000);
+camera.position.set(0, 0, 350);
 
 const init = () => {
 
-  console.log(`Hello, Project`);
+  renderer.render(scene, camera);
+  console.log(TweenMax, Power0);
+
 
 };
 
